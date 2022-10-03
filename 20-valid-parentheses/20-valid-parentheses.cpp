cfb7b836db6ca653a76d0a7659b1a196705c0bf1
himanishu-2512 <96021680+himanishu-2512@users.noreply.github.com>
2022-10-03 11:46:43 +0530

    Time: 5 ms (19.80%), Space: 6.2 MB (79.75%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,29 +2,35 @@ class Solution {
 public:
     bool isValid(string s) {
       stack<char> st;
+        if((strlen(&s[0]))%2!=0){
+            return false;
+        }
         for(int i=0;i<strlen(&s[0]);i++){
             if(s[i]=='('||s[i]=='['||s[i]=='{')
             st.push(s[i]);
-            else if(s[i]==')'&&!st.empty()){
-                if(st.top()=='(')
-                st.pop();
-                   else{
-                    return false;
+            else if(!st.empty()){
+                
+                switch(st.top()){
+                    case '(':
+                        if(s[i]!=')')
+                            return false;
+                        else st.pop();
+                        break;
+                    case '{':
+                        if(s[i]!='}')
+                            return false;
+                        else
+                            st.pop();
+                        break;
+                    case '[':
+                        if(s[i]!=']')
+                            return false;
+                        else st.pop();
+                        break;
+                    default:return false;
+                    
                 }
-            }
-            else if(s[i]==']'&&!st.empty()){
-                if(st.top()=='[')
-                st.pop();
-                   else{
-                    return false;
-                }
-            }
-            else if(s[i]=='}'&&!st.empty()){
-                if(st.top()=='{')
-                st.pop();
-                else{
-                    return false;
-                }
+               
             }
             else{
                 return false;
