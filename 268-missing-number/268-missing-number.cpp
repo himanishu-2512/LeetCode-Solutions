a14674d67b722ee21746be8724d16d6a8d597e40
himanishu-2512 <96021680+himanishu-2512@users.noreply.github.com>
2022-10-09 13:27:27 +0530

    Time: 25 ms (81.63%), Space: 17.8 MB (93.54%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,7 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
         int a=0;
         for(int i=0;i<nums.size();i++){
-            a=nums[i]^(i+1)^a;
+            a^=nums[i]^(i+1);
         }
         return a;
     }
