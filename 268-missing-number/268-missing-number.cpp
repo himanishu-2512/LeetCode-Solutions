50293ccc1cac8f07413343261358aa2008329fd6
himanishu-2512 <96021680+himanishu-2512@users.noreply.github.com>
2022-10-09 13:23:07 +0530

    Time: 61 ms (12.38%), Space: 18.1 MB (20.32%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int a=0;
+        for(int i=0;i<nums.size();i++){
+            a=nums[i]^(i+1)^a;
+        }
+        return a;
+    }
+};
