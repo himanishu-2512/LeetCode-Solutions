5f5953cf87e38fa3cc36ff0e2829fbdd058672de
Himanshu Singh <himanshu64.ugcs20@iiitranchi.ac.in>
2023-06-21 11:35:10 +0530

    Time: 7 ms (17.19%) | Memory: 5.9 MB (93.05%) - LeetSync

---
+++ add-digits/add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+      
+        int sum=0;
+        while(num>=10){
+           int n=num;
+            while(n>0){
+                sum+=n%10;
+                n=n/10;
+            }
+            num=sum;
+            sum=0;
+        }
+        return num;
+    }
+};
