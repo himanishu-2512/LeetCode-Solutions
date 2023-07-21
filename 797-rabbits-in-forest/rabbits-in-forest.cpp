545d653d48e8f57627e9e14a24a014f9480eea78
Himanshu Singh <himanshu64.ugcs20@iiitranchi.ac.in>
2023-07-21 11:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (6.66%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        map<int,int> m;
+        int count=0;
+        for(int i:answers){
+            if(m[i]==i){
+                m[i]++;
+                count+=m[i];
+                m[i]=0;
+            }
+            else{
+                m[i]++;
+            }
+        }
+        for(auto i:m){
+            if(i.second!=0)
+            count+=i.first+1;
+        }
+        return count;
+    }
+};
