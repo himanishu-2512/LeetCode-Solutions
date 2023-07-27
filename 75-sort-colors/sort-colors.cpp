1ce08f3c2bc2130e5c3cd8e969de7c9b9a77eb73
Himanshu Singh <himanshu64.ugcs20@iiitranchi.ac.in>
2023-07-27 17:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (80.62%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0,m=0,h=nums.size()-1;
+       while(m<=h){
+          if(nums[m]==0){
+              swap(nums[m],nums[l]);
+            l++;
+            m++;
+          }
+          else if(nums[m]==2){
+              swap(nums[h],nums[m]);
+            h--;
+          }
+          else{m++;}
+          
+
+        }
+
+    }
+};
