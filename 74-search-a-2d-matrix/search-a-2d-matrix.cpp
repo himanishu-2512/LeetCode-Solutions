dfa8a92e6d8dd0c6ccb1916dbb5bb2901364c6c5
Himanshu Singh <himanshu64.ugcs20@iiitranchi.ac.in>
2023-08-22 19:24:26 +0530

    Time: 7 ms (20.29%) | Memory: 9.6 MB (27.73%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,14 +3,17 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size();
         int n=matrix[0].size();
-        for(int i=0;i<matrix.size();i++){
-            if(target>=matrix[i][0]&&target<=matrix[i][n-1]){
-                for(int j=0;j<n;j++){
-                    if(target==matrix[i][j]){
-                        return true;
-                    }
-                }
-                return false;
+        int low=0,high=m*n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(matrix[mid/n][mid%n]==target){
+                return true;
+            }
+            else if(matrix[mid/n][mid%n]<target){
+            low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return false;
