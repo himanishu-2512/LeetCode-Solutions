4f4b001c0b0e05dc2d59b3f41916c33b1a57a649
Himanshu Singh <himanshu64.ugcs20@iiitranchi.ac.in>
2023-09-17 19:15:42 +0530

    Time: 3 ms (43.68%) | Memory: 6.6 MB (5.32%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        st.push('1');
-        for(int i=0;i<s.size();i++){
-            if(s[i]==')'||s[i]=='}'||s[i]==']'){
-                if(s[i]==')'&&st.top()=='('){
-                    st.pop();
-                }
-                else if(s[i]=='}'&&st.top()=='{'){
-                    st.pop();
-                }
-                else if(s[i]==']'&&st.top()=='['){
-                    st.pop();
-                }
-                else return false;
-            }
-            else {
-                st.push(s[i]);
-            }
+        stack<char> stack;
+        stack.push('-1');
+        for(int i=0;i<s.length();i++){
+          if(s[i]=='('||s[i]=='{'||s[i]=='['){
+              stack.push(s[i]);
+          }
+          else{
+             if(!(s[i]==')'&&stack.top()=='(')&&!(stack.top()=='{'&&s[i]=='}')&&!(stack.top()=='['&&s[i]==']'))return false;
+             else stack.pop();
+          }
         }
-        if(st.top()=='1')
+        if(stack.size()!=1)return false;
         return true;
-        return false;
     }
 };
